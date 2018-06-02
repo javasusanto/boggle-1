978221b04e41ac6260546d5ab8d6dc5fdf6c9bfd
rizariza69 <riza.riza69@gmail.com>
2018-06-02 23:33:22 +0700

    boggle 1

--- boggle.js
+++ boggle.js
@@ -1,23 +1,44 @@
-// ALGORITMA
-// 1. Buat board Boggle
-//    1.1 buat function board
-//    1.2 masukan kamus abjad dengn isi abjad
-//    1.3 masukan boxpenampung (boxArr)
-//    1.4 buat perulangan i untuk membuathasil Array, di push ke box penampung
-//    1.4 buat perulang j untuk memasukan hasil random dari kamus alpabet.
-// 2. buat solve PROBLEM
-//    2.1 buat function solveboggle
 
+// function board(angka) {
+//     let kamusAbjad = 'ABCDEFGHIJLMNOPQRESTUVWXYZ'
+//     let boxArr = []
+//     for (let i = 0; i < angka; i++) {
+//         boxArr.push([])
+//         for (let j = 0; j < angka; j++) {
+//             boxArr[i].push(kamusAbjad[Math.floor(Math.random()*26)])
+//         }
+//     }
+//     return boxArr
+//   }
+//   console.log(board(4))
 
-function board(angka) {
-    let kamusAbjad = 'ABCDEFGHIJLMNOPQRESTUVWXYZ'
-    let boxArr = []
-    for (let i = 0; i < angka; i++) {
-        boxArr.push([])
-        for (let j = 0; j < angka; j++) {
-            boxArr[i].push(kamusAbjad[Math.floor(Math.random()*26)])
-        }
+class boggle {
+
+  constructor(abjadString) {
+    this.abjad = abjadString
+  }
+
+  randomAbjad(){
+    let abjad = this.abjad
+    return abjad.charAt(Math.floor(Math.random() * abjad.length))
+  }
+
+  board(row,col) {
+    let boxArr = [];
+
+    for (let i = 0; i < col; i++) {
+      boxArr.push([]);
+      for (let j = 0; j < row; j++) {
+        boxArr[i].push(this.randomAbjad());
+
+      }
     }
     return boxArr
   }
-  console.log(board(4))
+
+}
+
+let abjadString = 'ABCDEFGHAIJKLMNOPQRSTUVWXYZ'
+let game = new boggle(abjadString)
+
+console.log(game.board(4,5));
