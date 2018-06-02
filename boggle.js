35fbf449dbd76e9bd0bf19318371a23b33b86df9
dawimuhammad <muhamadhaddawi@gmail.com>
2018-06-02 23:48:50 +0700

    Haddawi - Release 0 - Refactor release 0

--- boggle.js
+++ boggle.js
@@ -18,10 +18,8 @@ class Boggle {
         for (let i = 0; i < this.boggleLength; i++) {
             let rowBoggle = [];
 
-
-
             for (let j = 0; j < this.boggleLength; j++) {
-                rowBoggle.push('[ '+this.randomAlphabet()+' ] ');
+                rowBoggle.push(this.randomAlphabet());
             }
 
             this.boggle.push(rowBoggle);
@@ -30,19 +28,25 @@ class Boggle {
         this.printBoggle();
     }
 
+    // method to print boggle board on command line
     printBoggle() {
         console.log('   --- Boggle Board Game ---\n');
+
         for (let i = 0; i < this.boggle.length; i++) {
-            // for (let j = 0; j < this.boggle[i].length; j++) {
-                console.log('    '+this.boggle[i].join(''));
-            // }
+            console.log('         '+this.boggle[i].join('  '));
         }
     }
 
+    // method to create random alphabet
     randomAlphabet() {
         let alphabets = 'abcdefghijklmnopqrstuvwxyz';
 
         return alphabets.charAt(Math.floor(Math.random() * 26) + 0);
+    }
+
+    // method to solve boggle game
+    solve() {
+
     }
 
 }
